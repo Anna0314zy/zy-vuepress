d260937ed6b1b937ea75b7c7766a50b1a90ad725
zy <891374900@qq.com>
2025-02-13 16:50:04 +0800

    Feat: [init] #init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,159 @@
+import { blogPlugin } from '@vuepress/plugin-blog'
+import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+  lang: 'en-US',
+
+  title: 'VuePress',
+  description: 'My first VuePress Site',
+  author: "zy",
+  theme: defaultTheme({
+    logo: 'https://vuejs.press/images/hero.png',
+
+    navbar: [
+      '/',
+      // {
+      //   text: "Shark Xu 的博客",
+      //   items: [
+      //     { text: "掘金", link: "https://juejin.cn/user/233526039432445" },
+      //     { text: "Github", link: "https://github.com/Xusssyyy" }
+      //   ]
+      // },
+      {
+        text: 'Article',
+        link: '/article/',
+      },
+      {
+        text: 'Category',
+        link: '/category/',
+      },
+      {
+        text: 'Tag',
+        link: '/tag/',
+      },
+      {
+        text: 'Timeline',
+        link: '/timeline/',
+      },
+     
+    ],
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: "博客", // 默认文案 “分类”
+      },
+      tag: {
+        location: 4, // 在导航栏菜单中所占的位置，默认4
+        text: "Tag", // 默认文案 “标签”
+      },
+    },
+  }),
+
+  plugins: [
+    blogPlugin({
+      // Only files under posts are articles
+      filter: ({ filePathRelative }) =>
+        filePathRelative ? filePathRelative.startsWith('posts/') : false,
+
+      // Getting article info
+      getInfo: ({ frontmatter, title, data }) => ({
+        title,
+        author: frontmatter.author || '',
+        date: frontmatter.date || null,
+        category: frontmatter.category || [],
+        tag: frontmatter.tag || [],
+        excerpt:
+          // Support manually set excerpt through frontmatter
+          typeof frontmatter.excerpt === 'string'
+            ? frontmatter.excerpt
+            : data?.excerpt || '',
+      }),
+
+      // Generate excerpt for all pages excerpt those users choose to disable
+      excerptFilter: ({ frontmatter }) =>
+        !frontmatter.home &&
+        frontmatter.excerpt !== false &&
+        typeof frontmatter.excerpt !== 'string',
+
+      category: [
+        {
+          key: 'category',
+          getter: (page) => page.frontmatter.category || [],
+          layout: 'Category',
+          itemLayout: 'Category',
+          frontmatter: () => ({
+            title: 'Categories',
+            sidebar: false,
+          }),
+          itemFrontmatter: (name) => ({
+            title: `Category ${name}`,
+            sidebar: false,
+          }),
+        },
+        {
+          key: 'tag',
+          getter: (page) => page.frontmatter.tag || [],
+          layout: 'Tag',
+          itemLayout: 'Tag',
+          frontmatter: () => ({
+            title: 'Tags',
+            sidebar: false,
+          }),
+          itemFrontmatter: (name) => ({
+            title: `Tag ${name}`,
+            sidebar: false,
+          }),
+        },
+      ],
+
+      type: [
+        {
+          key: 'article',
+          // Remove archive articles
+          filter: (page) => !page.frontmatter.archive,
+          layout: 'Article',
+          frontmatter: () => ({
+            title: 'Articles',
+            sidebar: false,
+          }),
+          // Sort pages with time and sticky
+          sorter: (pageA, pageB) => {
+            if (pageA.frontmatter.sticky && pageB.frontmatter.sticky)
+              return pageB.frontmatter.sticky - pageA.frontmatter.sticky
+
+            if (pageA.frontmatter.sticky && !pageB.frontmatter.sticky) return -1
+
+            if (!pageA.frontmatter.sticky && pageB.frontmatter.sticky) return 1
+
+            if (!pageB.frontmatter.date) return 1
+            if (!pageA.frontmatter.date) return -1
+
+            return (
+              new Date(pageB.frontmatter.date).getTime() -
+              new Date(pageA.frontmatter.date).getTime()
+            )
+          },
+        },
+        {
+          key: 'timeline',
+          // Only article with date should be added to timeline
+          filter: (page) => page.frontmatter.date instanceof Date,
+          // Sort pages with time
+          sorter: (pageA, pageB) =>
+            new Date(pageB.frontmatter.date).getTime() -
+            new Date(pageA.frontmatter.date).getTime(),
+          layout: 'Timeline',
+          frontmatter: () => ({
+            title: 'Timeline',
+            sidebar: false,
+          }),
+        },
+      ],
+      hotReload: true,
+    }),
+  ],
+
+  bundler: viteBundler(),
+})
