b1e71d96a988909f75c0a8b87f56da94a56bca09
zy <891374900@qq.com>
2025-04-16 17:18:36 +0800

    Feat: add

--- src/posts/算法/1.js
+++ src/posts/算法/1.js
@@ -1,3 +1,17 @@
 // 测试二分法
 
-const arr = [10,1,2];
+// 删除数组的某一个元素
+
+const arr = [1];
+// 删除数组重复元素
+function removeDuplicates(arr) {
+    if(arr.length === 0) return 0;
+    let slow = 1;
+    for(let fast = 1; fast < arr.length; fast++) {
+        if(arr[fast] !== arr[slow - 1]) {
+            arr[slow++] = arr[fast];
+        }
+    }
+    return slow;
+}
+console.log(removeDuplicates(arr))
